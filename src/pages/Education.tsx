7eba6084e2db9a96f2947c691dc8388fac3335ca
MR-VL <cosmmo6@gmail.com>
2025-06-25 19:49:02 -0500

    education page template

--- src/pages/Education.tsx
+++ src/pages/Education.tsx
@@ -2,12 +2,37 @@ import React from 'react';
 
 const Education: React.FC = () => {
     return (
-        <div>
-            <p>This is the education page</p>
+        <div className="container mt-5 text-white">
+            <h1 className="mb-4 border-bottom pb-2">Education</h1>
+
+            <div className="border-bottom border-secondary pb-2 mb-5">
+                <h3 className="text-info">University of Example B</h3>
+                <p className="mb-1"><strong>Degree:</strong> ddd</p>
+                <p className="mb-1"><strong>GPA:</strong> 0</p>
+                <p className="mb-1"><strong>Graduation Year:</strong> 1233</p>
+                <p className="mb-1"><strong>Academic Achievements:</strong> woof</p>
+                <p className="text-light"><strong>Focused on: </strong>kdjkfd</p>
+            </div>
+
+            <div className="border-bottom border-secondary pb-2 mb-5">
+                <h3 className="text-info">University of Example B</h3>
+                <p className="mb-1"><strong>Degree:</strong> ddd</p>
+                <p className="mb-1"><strong>GPA:</strong> 0</p>
+                <p className="mb-1"><strong>Graduation Year:</strong> 1233</p>
+                <p className="mb-1"><strong>Academic Achievements:</strong> woof</p>
+                <p className="text-light"><strong>Focused on: </strong>kdjkfd</p>
+            </div>
+
+            <div>
+                <h3 className="text-info">University of Example B</h3>
+                <p className="mb-1"><strong>Degree:</strong> ddd</p>
+                <p className="mb-1"><strong>GPA:</strong> 0</p>
+                <p className="mb-1"><strong>Graduation Year:</strong> 1233</p>
+                <p className="mb-1"><strong>Academic Achievements:</strong> woof</p>
+                <p className="text-light"><strong>Focused on: </strong>kdjkfd</p>
+            </div>
         </div>
-
     );
-
 };
 
 export default Education;
